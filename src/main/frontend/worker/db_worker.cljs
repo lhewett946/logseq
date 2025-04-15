611c232361eec5ac39c78c88ea1eb3583114981c
Tienson Qin <tiensonqin@gmail.com>
2025-04-16 00:37:08 +0800

    per-tab undo redo

--- src/main/frontend/worker/db_worker.cljs
+++ src/main/frontend/worker/db_worker.cljs
@@ -26,8 +26,7 @@
             [frontend.worker.rtc.db-listener]
             [frontend.worker.search :as search]
             [frontend.worker.shared-service :as shared-service]
-            [frontend.worker.state :as worker-state] ;; [frontend.worker.undo-redo :as undo-redo]
-            [frontend.worker.undo-redo2 :as undo-redo]
+            [frontend.worker.state :as worker-state]
             [frontend.worker.util :as worker-util]
             [goog.object :as gobj]
             [lambdaisland.glogi.console :as glogi-console]
@@ -716,11 +715,6 @@
   (worker-state/set-new-state! new-state)
   nil)
 
-(def-thread-api :thread-api/sync-ui-state
-  [repo state]
-  (undo-redo/record-ui-state! repo (ldb/write-transit-str state))
-  nil)
-
 (def-thread-api :thread-api/export-get-debug-datoms
   [repo]
   (when-let [db (worker-state/get-sqlite-conn repo)]
@@ -737,20 +731,15 @@
   (when-let [conn (worker-state/get-datascript-conn repo)]
     (worker-export/get-all-page->content repo @conn)))
 
-(def-thread-api :thread-api/undo
-  [repo _page-block-uuid-str]
-  (when-let [conn (worker-state/get-datascript-conn repo)]
-    (undo-redo/undo repo conn)))
+;; (def-thread-api :thread-api/undo
+;;   [repo _page-block-uuid-str]
+;;   (when-let [conn (worker-state/get-datascript-conn repo)]
+;;     (undo-redo/undo repo conn)))
 
-(def-thread-api :thread-api/redo
-  [repo _page-block-uuid-str]
-  (when-let [conn (worker-state/get-datascript-conn repo)]
-    (undo-redo/redo repo conn)))
-
-(def-thread-api :thread-api/record-editor-info
-  [repo _page-block-uuid-str editor-info]
-  (undo-redo/record-editor-info! repo editor-info)
-  nil)
+;; (def-thread-api :thread-api/redo
+;;   [repo _page-block-uuid-str]
+;;   (when-let [conn (worker-state/get-datascript-conn repo)]
+;;     (undo-redo/redo repo conn)))
 
 (def-thread-api :thread-api/validate-db
   [repo]
@@ -893,7 +882,7 @@
                                         method-k (keyword (first args))]
                                     ;; what about undo and redo?
                                     (cond
-                                      (or (contains? #{:thread-api/init-shared-service :thread-api/sync-app-state :thread-api/sync-ui-state :thread-api/record-editor-info} method-k)
+                                      (or (contains? #{:thread-api/init-shared-service :thread-api/sync-app-state} method-k)
                                           (nil? service)
                                           @shared-service/*provider?)
                                       (apply f args)
